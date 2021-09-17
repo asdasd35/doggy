8f1ff71f0a4c3a970cd11c52993945f9a45ff419
asdasd35 <88936560+asdasd35@users.noreply.github.com>
2021-09-17 16:48:51 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+ 
+const prefix = "do"
+ 
+bot.login("ODYwMTEyMDUxMzgwMjg5NTc2.YN2fxQ.wcB686ygBn0LpHOi0l6qOkgmcg8")
+ 
+bot.on("ready", () => {
+    console.log("Bejelentkezve: " + bot.user.tag)
+    bot.user.setActivity("DOGGY", { type: "WATCHING" })
+})
+ 
+bot.on("message", message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) { return }
+ 
+    let args = message.content.substring(prefix.length).split(" ")
+ 
+    switch (args[0]) {
+        case "ggy":
+            message.channel.send("https://cdn.portfolio.hu/articles/images-o/d/o/g/dogememe-444964.png")
+            message.author.send("**#DOGE MEME**")
+            break
+        case "ggydm":
+            let user = message.mentions.users.first()
+            user.send("▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n**Join the new giveaway server!**\nhttps://discord.gg/yNnQpHEn6S\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬")
+            break
+        default:
+            message.channel.send("**Mindenki doggy-t a chatbe!**")
+            break
+    }
+})
